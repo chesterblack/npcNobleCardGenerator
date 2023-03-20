5b78b710559e493814e4439dc8f8735a9ec98f0d
chesterblack <chez.black@hotmail.co.uk>
2023-03-20 18:47:57 +0000

    Preload portrait images

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,20 +1,25 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-
 import { npcs } from '@/lib/data';
 import Card from '@/components/Card';
-import Selector from '@/components/Selector';
-import { useState } from 'react';
 
 export default function Home() {
-	const router = useRouter();
 	const firstNpc = Object.entries(npcs)[0][0];
 	const [npcName, setNpcName] = useState(firstNpc);
+
+	let preloadedImages = [];
+	for (const [key, npc] of Object.entries(npcs)) {
+		preloadedImages.push(
+			<link rel="preload" href={npc.portrait} as="image" />
+		);
+	}
 
 	return (
 		<>
 			<Head>
 				<title>Noble NPC Card Generator</title>
+				{preloadedImages}
 			</Head>
 			<main>
 				<Card npcName={npcName} setNpcName={setNpcName} />
