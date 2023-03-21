d9d8aae0bfb2974013ad07cd5ba18392980da9dc
chesterblack <chez.black@hotmail.co.uk>
2023-03-21 09:47:26 +0000

    Adds routing for putting npc name in first segment

--- app/src/components/NpcMenu.jsx
+++ app/src/components/NpcMenu.jsx
@@ -1,11 +1,21 @@
 import { npcs } from '@/lib/data';
+import { useRouter } from 'next/router';
 
 export default function NpcMenu({ setNpcName, menuOpen, setMenuOpen }) {
+	const router = useRouter();
 	let options = [];
 
 	function handleClick(key) {
 		setNpcName(key);
 		setMenuOpen(false);
+
+		router.push(
+			{
+				pathname: `/${key}`,
+			},
+			undefined,
+			{ shallow: true }
+		);
 	}
 
 	for (const [key, npc] of Object.entries(npcs)) {
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { npcs } from '@/lib/data';
 import Card from '@/components/Card';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+	const router = useRouter();
 	const firstNpc = Object.entries(npcs)[0][0];
 	const [npcName, setNpcName] = useState(firstNpc);
+
+	useEffect(() => {
+		if (router.query.npc) {
+			setNpcName(router.query.npc);
+		}
+	}, [router.query]);
 
 	let preloadedImages = [];
 	for (const [key, npc] of Object.entries(npcs)) {
@@ -19,6 +26,10 @@ export default function Home() {
 		<>
 			<Head>
 				<title>Noble NPC Card Generator</title>
+				<link
+					rel="shortcut icon"
+					href="/images/arms/Kingdom_of_Sundria.svg"
+				/>
 				{preloadedImages}
 			</Head>
 			<main>
